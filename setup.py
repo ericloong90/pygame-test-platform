39280f00ec6f0878953297cec035ccfd82fc19e5
Eric Tai Chi Loong <ericloong90@gmail.com>
2017-04-14 23:02:48 +0800

    Rebuilding package for consistency.

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ executables = [cx_Freeze.Executable("platform_sol.py")]
 
 cx_Freeze.setup(
     name = "My First Game!",
-
     options = {"build_exe": {"packages": ["pygame"], "include_files": [("./assets/background.png", "./assets/background.png"), ("./assets/dirt_32x32.png", "./assets/dirt_32x32.png"), ("./assets/grass_32x32.png", "./assets/grass_32x32.png"), ("./assets/jump.wav", "./assets/jump.wav"), ("./assets/player.png", "./assets/player.png")]}},
-    
     executables = executables
     )
