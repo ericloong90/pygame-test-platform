f9804a87236d7c5e2318f77843961afe7c3eaa27
Eric Tai Chi Loong <ericloong90@gmail.com>
2017-04-14 14:10:07 +0800

    First successful packaging with assets in subdirectories.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import cx_Freeze
 
-executables = [cx_Freeze.Executable("your-script-name.py")]
+executables = [cx_Freeze.Executable("platform_sol.py")]
 
 cx_Freeze.setup(
     name = "My First Game!",
 
-    options = {"build_exe": {"packages": ["pygame"], "include_files": []}},
+    options = {"build_exe": {"packages": ["pygame"], "include_files": [("./assets/background.png", "./assets/background.png"), ("./assets/dirt_32x32.png", "./assets/dirt_32x32.png"), ("./assets/grass_32x32.png", "./assets/grass_32x32.png"), ("./assets/jump.wav", "./assets/jump.wav"), ("./assets/player.png", "./assets/player.png")]}},
     
     executables = executables
     )
